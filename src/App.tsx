e2c2db39559e653b7f124b71b0f4bb6f0ef37be0
twknab <20636750+twknab@users.noreply.github.com>
2024-06-23 12:42:28 -0700

    remove boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import './App.css'
 function App() {
   return (
     <div>
+      {/* TODO: Frame out component */}
+      {/* Add "Add Contact" button */}
+      {/* Add "Contact List" */}
+      {/* Clicking into Contact List shows Details */}
+      {/* Optional: Add "Edit Contact" button */}
+      {/* Optional: Add "Delete Contact" button */}
       <h1>Hello World</h1>
     </div>
   )
