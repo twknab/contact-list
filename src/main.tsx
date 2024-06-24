32d39f866f2bbc607f66f0f06d90712bf4de3186
twknab <20636750+twknab@users.noreply.github.com>
2024-06-23 18:43:51 -0700

    chore: Update package-lock.json and tsconfig.app.json for new dependencies.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './output.css'
+import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
