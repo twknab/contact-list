6e2bf2ae17c422f96ddd60b506d3ddb33e46be7e
twknab <20636750+twknab@users.noreply.github.com>
2024-06-23 18:48:01 -0700

    feat: Add routing for ContactDetails and ContactList views.

--- src/views/ContactList.tsx
+++ src/views/ContactList.tsx
@@ -1,5 +1,27 @@
-import { VStack } from "@chakra-ui/react";
+import { Heading, VStack } from "@chakra-ui/react";
 import ContactCard from "../components/ContactCard";
+
+export interface Contact {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  notes: string;
+}
+
+// export interface Contacts {
+//   contacts: Contact[];
+// }
+
+const contact = {
+  id: "1",
+  firstName: "John",
+  lastName: "Doe",
+  email: "john.doe@example.com",
+  phoneNumber: "1234567890",
+  notes: "Qui ea duis fugiat cupidatat elit sit."
+};
 
 function ContactList() {
   return (
@@ -8,10 +30,11 @@ function ContactList() {
       {/* Clicking into Contact List shows Details */}
       {/* Optional: Add "Edit Contact" button */}
       {/* Optional: Add "Delete Contact" button */}
-      <h1>Contact List</h1>
+      <Heading mb="6">Contact List</Heading>
       {/* TODO: Iterate over data and render contact card for each */}
-      <ContactCard />
-      <ContactCard />
+      {/* TODO: Handle a no data state */}
+      <ContactCard contact={contact} />
+      <ContactCard contact={contact} />
     </VStack>
   );
 }
